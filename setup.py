ce737d7ecdee6e25fade4acd793787a17617335f
Apo08K <aroranik452@gmail.com>
2025-05-21 00:14:31 +0530

    .

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from setuptools import setup, find_packages
+#this file helps us in installing local packages in our environment
+from setuptools import setup, find_packages 
 
 setup(
     name="src",
     version="0.0.1",
     author="Vikash Das",
     author_email="vikashdas770@gmail.com",
-    packages=find_packages()
+    packages=find_packages() #will find package under that name "src"
 )
